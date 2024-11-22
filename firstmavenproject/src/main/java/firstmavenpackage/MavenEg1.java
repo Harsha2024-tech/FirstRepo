b3e4e723a75515bac830c5e6af3fe0d0cfbed6d9
Vivek <Vivek@DESKTOP-VNQKMTA>
2024-11-22 11:59:50 +0530

    my sample commit

---
+++ firstmavenproject/src/main/java/firstmavenpackage/MavenEg1.java
@@ -0,0 +1,19 @@
+package firstmavenpackage;
+
+public class MavenEg1 {
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addition result:"+c);
+	}
+
+	public static void main(String[] args) {
+		MavenEg1 obj=new MavenEg1();
+		obj.add();
+		// TODO Auto-generated method stub
+
+	}
+
+}
