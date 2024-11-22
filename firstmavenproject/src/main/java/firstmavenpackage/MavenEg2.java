66b462d0fa49089f87fb9c47435dab471c13a3e6
Vivek <Vivek@DESKTOP-VNQKMTA>
2024-11-22 12:07:57 +0530

    My 2nd java

---
+++ firstmavenproject/src/main/java/firstmavenpackage/MavenEg2.java
@@ -0,0 +1,18 @@
+package firstmavenpackage;
+
+public class MavenEg2 {
+	public static void multi()
+	{
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println("Multiplication result:" +c);
+	}
+
+	public static void main(String[] args) {
+		MavenEg2.multi();
+		// TODO Auto-generated method stub
+
+	}
+
+}
